8266edcabae4069ba62f0a9ae5978f180a32d792
William Astorga <wastorga@altus.co.cr>
2021-08-26 09:47:21 -0600

    Update resource_allowed_protocol

--- examples/resource_allowed_protocols/main.tf
+++ examples/resource_allowed_protocols/main.tf
@@ -24,7 +24,6 @@ resource "ciscoise_allowed_protocols" "response" {
     allow_leap                   = false
     allow_eap_ttls               = false
     allow_eap_fast               = false
-    allow_peap                   = false
     allow_teap                   = false
     allow_preferred_eap_protocol = false
     eap_tls_l_bit                = false
@@ -34,7 +33,7 @@ resource "ciscoise_allowed_protocols" "response" {
     # Set to true if eap_tls is uncommented
     allow_eap_tls = true
     # Set to false if eap_tls is commented
-    # allow_eap_tls                = false
+    # allow_eap_tls = false
     eap_tls {
       allow_eap_tls_auth_of_expired_certs = false
       # Set to false if following args are commented
@@ -42,10 +41,27 @@ resource "ciscoise_allowed_protocols" "response" {
       # Set to true if following args are uncommented
       # eap_tls_enable_stateless_session_resume = true
       # Following args
-      # eap_tls_session_ticket_precentage       = 1
-      # eap_tls_session_ticket_ttl              = 1
-      # eap_tls_session_ticket_ttl_units        = "SECONDS"
+      # eap_tls_session_ticket_precentage = 1
+      # eap_tls_session_ticket_ttl        = 1
+      # eap_tls_session_ticket_ttl_units  = "SECONDS"
     }
+    # Set to true if peap is uncommented
+    allow_peap = true
+    # Set to false if eap_tls is commented
+    # allow_peap = false
+    peap {
+      allow_peap_eap_ms_chap_v2                    = true
+      allow_peap_eap_ms_chap_v2_pwd_change         = true
+      allow_peap_eap_ms_chap_v2_pwd_change_retries = 1
+      allow_peap_eap_gtc                           = true
+      allow_peap_eap_gtc_pwd_change                = true
+      allow_peap_eap_gtc_pwd_change_retries        = 1
+      allow_peap_eap_tls                           = true
+      allow_peap_eap_tls_auth_of_expired_certs     = false
+      require_cryptobinding                        = false
+      allow_peap_v0                                = false
+    }
+
   }
 }
 
