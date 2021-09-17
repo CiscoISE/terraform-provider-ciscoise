7cb0c52bfae2de21e152a831c431b897f21861d0
William Astorga <wastorga@altus.co.cr>
2021-09-17 11:48:05 -0600

    Add examples/samples

--- examples/samples/active_directory/main.tf
+++ examples/samples/active_directory/main.tf
@@ -15,15 +15,66 @@ data "ciscoise_active_directory" "response" {
   page     = 1
   size     = 20
 }
-output "ciscoise__active_directory_response" {
+
+output "ciscoise_active_directory_response" {
   value = data.ciscoise_active_directory.response
 }
 
-data "ciscoise_active_directory" "single_response" {
-  provider = ciscoise
-  id       = data.ciscoise_active_directory.response.items[0].id
-}
+# data "ciscoise_active_directory" "single_response" {
+#   provider = ciscoise
+#   id       = data.ciscoise_active_directory.response.items[0].id
+# }
 
-output "ciscoise__active_directory_single_response" {
-  value = data.ciscoise_active_directory.single_response
+# output "ciscoise_active_directory_single_response" {
+#   value = data.ciscoise_active_directory.single_response
+# }
+
+resource "ciscoise_active_directory" "example" {
+  provider = ciscoise
+  item {
+    name =  "cisco.com"
+    description =  "Cisco AD"
+    domain =  "cisco.com"
+    adgroups {
+      groups {
+        name =  "cisco.com/operators"
+        sid =  "S-1-5-32-548"
+        type =  "GLOBAL"
+      }
+    } 
+    advanced_settings {
+      aging_time =  5
+      country  = "co"
+      # auth_protection_type = ""
+      department =  "department"
+      email =  "mail"
+      enable_callback_for_dialin_client =  false
+      enable_pass_change =  true
+      enable_machine_auth =  true
+      enable_machine_access =  true
+      enable_dialin_permission_check =  false
+      plaintext_auth =  false
+      identity_not_in_ad_behaviour =  "SEARCH_JOINED_FOREST"
+      unreachable_domains_behaviour =  "PROCEED"
+      enable_rewrites =  false
+      first_name =  "givenName"
+      last_name =  "sn"
+      organizational_unit =  "company"
+      job_title =  "title"
+      locality =  "l"
+      state_or_province =  "st"
+      telephone =  "00171240591"
+      street_address =  "streetAddress"
+      schema =  "ACTIVE_DIRECTORY"
+    }
+    ad_attributes {
+      attributes {
+        name =  "name1"
+        type =  "STRING"
+        default_value =  "defaultString"
+        internal_name =  "internalName1"
+      }
+    }
+    ad_scopes_names =  "Default_Scope"
+  }
 }
