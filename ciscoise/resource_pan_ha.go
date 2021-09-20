b20ff1390bf3b5ade448e7e9b5a6d0f37b426668
William Astorga <wastorga@altus.co.cr>
2021-09-20 17:06:20 -0600

    Change bool to string @resource_pan_ha

--- ciscoise/resource_pan_ha.go
+++ ciscoise/resource_pan_ha.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"reflect"
 
-	"github.com/CiscoISE/ciscoise-go-sdk/sdk"
 	"log"
+
+	isegosdk "github.com/CiscoISE/ciscoise-go-sdk/sdk"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -14,12 +15,14 @@ import (
 func resourcePanHa() *schema.Resource {
 	return &schema.Resource{
 		Description: `It manages create, read and delete operations on PAN HA.
-  
-  - To deploy the auto-failover feature, you must have at least three nodes, where two of the nodes assume the
-  Administration persona, and one node acts as the health check node. A health check node is a non-administration node and
-  can be a Policy Service, Monitoring, or pxGrid node, or a combination of these. If the PANs are in different data
-  centers, you must have a health check node for each PAN.
-  - Disable the automatic PAN failover`,
+
+- To deploy the auto-failover feature, you must have at least three nodes, where two of the nodes assume the
+Administration persona, and one node acts as the health check node. A health check node is a non-administration node and
+can be a Policy Service, Monitoring, or pxGrid node, or a combination of these. If the PANs are in different data
+centers, you must have a health check node for each PAN.
+
+- Disable the automatic PAN failover
+`,
 
 		CreateContext: resourcePanHaCreate,
 		ReadContext:   resourcePanHaRead,
@@ -45,8 +48,10 @@ func resourcePanHa() *schema.Resource {
 							Optional: true,
 						},
 						"is_enabled": &schema.Schema{
-							Type:     schema.TypeBool,
-							Optional: true,
+							// Type:     schema.TypeBool,
+							Type:         schema.TypeString,
+							ValidateFunc: validateStringHasValueFunc([]string{"", "true", "false"}),
+							Optional:     true,
 						},
 						"polling_interval": &schema.Schema{
 							Type:     schema.TypeInt,
