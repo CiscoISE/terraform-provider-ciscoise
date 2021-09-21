e3855ccc7a6015c8188617101006f5d4d2fbe60f
William Astorga <wastorga@altus.co.cr>
2021-09-20 19:52:47 -0600

    Change bool to string @examples/samples

--- examples/samples/active_directory/main.tf
+++ examples/samples/active_directory/main.tf
@@ -32,49 +32,49 @@ output "ciscoise_active_directory_response" {
 resource "ciscoise_active_directory" "example" {
   provider = ciscoise
   item {
-    name =  "cisco.com"
-    description =  "Cisco AD"
-    domain =  "cisco.com"
+    name        = "cisco.com"
+    description = "Cisco AD"
+    domain      = "cisco.com"
     adgroups {
       groups {
-        name =  "cisco.com/operators"
-        sid =  "S-1-5-32-548"
-        type =  "GLOBAL"
+        name = "cisco.com/operators"
+        sid  = "S-1-5-32-548"
+        type = "GLOBAL"
       }
-    } 
+    }
     advanced_settings {
-      aging_time =  5
-      country  = "co"
+      aging_time = 5
+      country    = "co"
       # auth_protection_type = ""
-      department =  "department"
-      email =  "mail"
-      enable_callback_for_dialin_client =  false
-      enable_pass_change =  true
-      enable_machine_auth =  true
-      enable_machine_access =  true
-      enable_dialin_permission_check =  false
-      plaintext_auth =  false
-      identity_not_in_ad_behaviour =  "SEARCH_JOINED_FOREST"
-      unreachable_domains_behaviour =  "PROCEED"
-      enable_rewrites =  false
-      first_name =  "givenName"
-      last_name =  "sn"
-      organizational_unit =  "company"
-      job_title =  "title"
-      locality =  "l"
-      state_or_province =  "st"
-      telephone =  "00171240591"
-      street_address =  "streetAddress"
-      schema =  "ACTIVE_DIRECTORY"
+      department                        = "department"
+      email                             = "mail"
+      enable_callback_for_dialin_client = "false"
+      enable_pass_change                = "true"
+      enable_machine_auth               = "true"
+      enable_machine_access             = "true"
+      enable_dialin_permission_check    = "false"
+      plaintext_auth                    = "false"
+      identity_not_in_ad_behaviour      = "SEARCH_JOINED_FOREST"
+      unreachable_domains_behaviour     = "PROCEED"
+      enable_rewrites                   = "false"
+      first_name                        = "givenName"
+      last_name                         = "sn"
+      organizational_unit               = "company"
+      job_title                         = "title"
+      locality                          = "l"
+      state_or_province                 = "st"
+      telephone                         = "00171240591"
+      street_address                    = "streetAddress"
+      schema                            = "ACTIVE_DIRECTORY"
     }
     ad_attributes {
       attributes {
-        name =  "name1"
-        type =  "STRING"
-        default_value =  "defaultString"
-        internal_name =  "internalName1"
+        name          = "name1"
+        type          = "STRING"
+        default_value = "defaultString"
+        internal_name = "internalName1"
       }
     }
-    ad_scopes_names =  "Default_Scope"
+    ad_scopes_names = "Default_Scope"
   }
 }
