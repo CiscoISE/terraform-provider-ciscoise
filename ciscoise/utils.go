681610d8edb240169c5a3b284248ca0010b0499f
William Astorga <wastorga@altus.co.cr>
2021-09-27 11:27:53 -0600

    Remove commented code for isEmptyValue

--- ciscoise/utils.go
+++ ciscoise/utils.go
@@ -50,16 +50,8 @@ func isEmptyValue(v reflect.Value) bool {
 	}
 
 	switch v.Kind() {
-	case reflect.Array, reflect.Map, reflect.Slice: //, reflect.String:
+	case reflect.Array, reflect.Map, reflect.Slice:
 		return v.Len() == 0
-	// case reflect.Bool:
-	// 	return !v.Bool()
-	// case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-	// 	return v.Int() == 0
-	// case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-	// 	return v.Uint() == 0
-	// case reflect.Float32, reflect.Float64:
-	// 	return v.Float() == 0
 	case reflect.Struct:
 		return v.IsZero()
 	case reflect.Interface, reflect.Ptr:
