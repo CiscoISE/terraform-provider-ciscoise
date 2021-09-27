4cabb95b2bebe9926a44c150bdb32b1ea973e216
William Astorga <wastorga@altus.co.cr>
2021-09-27 12:36:12 -0600

    Update examples/samples

--- examples/samples/resource_my_device_portal/main.tf
+++ examples/samples/resource_my_device_portal/main.tf
@@ -24,11 +24,15 @@ resource "ciscoise_my_device_portal" "example" {
   }
 }
 
-# data "ciscoise_my_device_portal" "items" {
-#   provider = ciscoise
-#   id       = "5e97b665-385f-4f3e-a9f0-721664ea7f06"
-# }
+output "ciscoise_my_device_portal_example" {
+  value = ciscoise_my_device_portal.example.id
+}
 
-# output "ciscoise_my_device_portal_items" {
-#   value = data.ciscoise_my_device_portal.items.item
-# }
+data "ciscoise_my_device_portal" "item" {
+  provider = ciscoise
+  id       = ciscoise_my_device_portal.example.item[0].id
+}
+
+output "ciscoise_my_device_portal_item" {
+  value = data.ciscoise_my_device_portal.item.item
+}
