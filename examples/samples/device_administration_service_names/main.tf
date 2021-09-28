e2af65dbe8975009cfa2b6fe8f570d180028b609
William Astorga <wastorga@altus.co.cr>
2021-09-27 18:34:38 -0600

    Add more examples/samples

---
+++ examples/samples/device_administration_service_names/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    ciscoise = {
+      version = "0.0.1-beta"
+      source  = "hashicorp.com/edu/ciscoise"
+    }
+  }
+}
+
+provider "ciscoise" {
+}
+
+data "ciscoise_device_administration_service_names" "example" {
+  provider = ciscoise
+}
+
+output "ciscoise_device_administration_service_names_example" {
+  value = data.ciscoise_device_administration_service_names.example.items
+}
