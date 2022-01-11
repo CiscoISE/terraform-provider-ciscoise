1d9e77b509b4e2664dfd81a776d6f2117e05ec2b
William Astorga <wastorga@altus.co.cr>
2022-01-11 00:04:40 -0600

    Clean up resource comments

--- ciscoise/resource_portal_theme.go
+++ ciscoise/resource_portal_theme.go
@@ -276,7 +276,7 @@ func resourcePortalThemeUpdate(ctx context.Context, d *schema.ResourceData, m in
 
 	selectedMethod := pickMethod([][]bool{method1, method2})
 	var vvID string
-	// NOTE: Consider adding getAllItems and search function to get missing params
+	// NOTE: Added getAllItems and search function to get missing params
 	if selectedMethod == 2 {
 		queryParams1 := isegosdk.GetPortalThemesQueryParams{}
 
