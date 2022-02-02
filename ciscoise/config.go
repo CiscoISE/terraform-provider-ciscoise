429b89c69e93d754fa54ee1d2e4ca97e88e3c972
William Astorga <wastorga@altus.co.cr>
2022-02-02 12:41:05 -0600

    Add single_request_timeout as a provider configuration argument.

--- ciscoise/config.go
+++ ciscoise/config.go
@@ -2,6 +2,7 @@ package ciscoise
 
 import (
 	"context"
+	"time"
 
 	isegosdk "github.com/CiscoISE/ciscoise-go-sdk/sdk"
 
@@ -12,13 +13,14 @@ import (
 // Config is the configuration structure used to instantiate a
 // new Cisco Identity Services Engine client.
 type Config struct {
-	BaseURL       string
-	Username      string
-	Password      string
-	Debug         string
-	SSLVerify     string
-	UseAPIGateway string
-	UseCSRFToken  string
+	BaseURL        string
+	Username       string
+	Password       string
+	Debug          string
+	SSLVerify      string
+	UseAPIGateway  string
+	UseCSRFToken   string
+	RequestTimeout int
 }
 
 // NewClient returns a new Cisco Identity Services Engine client.
@@ -32,6 +34,9 @@ func (c *Config) NewClient() (*isegosdk.Client, error) {
 		return client, err
 	}
 	client.RestyClient().SetLogger(createLogger())
+	if c.RequestTimeout > 0 {
+		client.RestyClient().SetTimeout(time.Duration(c.RequestTimeout) * time.Second)
+	}
 	return client, err
 }
 
@@ -39,13 +44,14 @@ func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}
 	var diags diag.Diagnostics
 
 	config := Config{
-		BaseURL:       d.Get("base_url").(string),
-		Username:      d.Get("username").(string),
-		Password:      d.Get("password").(string),
-		Debug:         d.Get("debug").(string),
-		SSLVerify:     d.Get("ssl_verify").(string),
-		UseAPIGateway: d.Get("use_api_gateway").(string),
-		UseCSRFToken:  d.Get("use_csrf_token").(string),
+		BaseURL:        d.Get("base_url").(string),
+		Username:       d.Get("username").(string),
+		Password:       d.Get("password").(string),
+		Debug:          d.Get("debug").(string),
+		SSLVerify:      d.Get("ssl_verify").(string),
+		UseAPIGateway:  d.Get("use_api_gateway").(string),
+		UseCSRFToken:   d.Get("use_csrf_token").(string),
+		RequestTimeout: d.Get("single_request_timeout").(int),
 	}
 
 	client, err := config.NewClient()
