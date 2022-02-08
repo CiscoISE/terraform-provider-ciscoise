fd2265567cb416e19d8e386c169ae359e7dccab1
William Astorga <wastorga@altus.co.cr>
2022-02-08 14:40:02 -0600

    Update examples

--- examples/resources/ciscoise_node_group_node/import.sh
+++ examples/resources/ciscoise_node_group_node/import.sh
@@ -1,1 +1,1 @@
-terraform import ciscoise_node_group_node.example "hostname:=string\\node_group_name:=string"
+terraform import ciscoise_node_group_node.example "hostname:=string\node_group_name:=string"
---
+++ examples/samples/resources/ciscoise_anc_endpoint/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    ciscoise = {
+      version = "0.2.0"
+      source  = "hashicorp.com/edu/ciscoise"
+    }
+  }
+}
+
+provider "ciscoise" {
+}
+
+
+resource "ciscoise_anc_endpoint" "example" {
+  provider = ciscoise
+  parameters {
+    # ip_address = ""
+    # mac_address = "DEBB.3567.0173"
+    mac_address = "D0:27:0F:E6:D7:14"
+    policy_name = "Test"
+  }
+}
+
+output "ciscoise_anc_endpoint_example" {
+  value = ciscoise_anc_endpoint.example.item
+}
