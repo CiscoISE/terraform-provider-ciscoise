5cf45b5ca8ecbc70cb43ff3ed2556df1e439422f
fmunoz <fmunoz@altus.cr>
2022-04-18 13:28:20 -0600

    resources added: "ciscoise_sg_mapping_deploy":                            resourceSgMappingDeploy(), 			"ciscoise_guest_user_suspend":                           resourceGuestUserSuspend(), 			"ciscoise_guest_user_reinstate":                         resourceGuestUserReinstate(), 			"ciscoise_guest_user_deny":                              resourceGuestUserDeny(), 			"ciscoise_guest_user_approve":                           resourceGuestUserApprove(), 			"ciscoise_endpoint_certificate":                         resourceEndpointCertificate(), 			"ciscoise_endpoint_register":                            resourceEndpointRegister(), 			"ciscoise_endpoint_deregister":                          resourceEndpointDeregister(),

---
+++ ciscoise/resource_endpoint_certificate.go
@@ -0,0 +1,182 @@
+package ciscoise
+
+import (
+	"context"
+	"reflect"
+
+	"log"
+
+	isegosdk "github.com/CiscoISE/ciscoise-go-sdk/sdk"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceEndpointCertificate() *schema.Resource {
+	return &schema.Resource{
+		Description: `It performs update operation on EndpointCertificate.
+- This data source action allows the client to create an endpoint certificate.
+`,
+
+		CreateContext: resourceEndpointCertificateCreate,
+		ReadContext:   resourceEndpointCertificateRead,
+		DeleteContext: resourceEndpointCertificateDelete,
+
+		Schema: map[string]*schema.Schema{
+			"last_updated": &schema.Schema{
+				Description: `Unix timestamp records the last time that the resource was updated.`,
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"item": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"parameters": &schema.Schema{
+				Type:     schema.TypeList,
+				Required: true,
+				MaxItems: 1,
+				MinItems: 1,
+				ForceNew: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"dirpath": &schema.Schema{
+							Description: `Directory absolute path in which to save the file.`,
+							Type:        schema.TypeString,
+							Required:    true,
+							ForceNew:    true,
+						},
+						"cert_template_name": &schema.Schema{
+							Description: `Name of an Internal CA template`,
+							Type:        schema.TypeString,
+							Optional:    true,
+							ForceNew:    true,
+						},
+						"certificate_request": &schema.Schema{
+							Description: `Key value map. Must have CN and SAN entries`,
+							Type:        schema.TypeList,
+							Optional:    true,
+							ForceNew:    true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+
+									"cn": &schema.Schema{
+										Description: `Matches the requester's User Name, unless the Requester is an ERS Admin.
+			ERS Admins are allowed to create requests for any CN`,
+										Type:     schema.TypeString,
+										Optional: true,
+										ForceNew: true,
+									},
+									"san": &schema.Schema{
+										Description: `Valid MAC Address, delimited by '-'`,
+										Type:        schema.TypeString,
+										Optional:    true,
+										ForceNew:    true,
+									},
+								},
+							},
+						},
+						"format": &schema.Schema{
+							Description: `Allowed values:
+			- PKCS12,
+			- PKCS12_CHAIN,
+			- PKCS8,
+			- PKCS8_CHAIN`,
+							Type:     schema.TypeString,
+							Optional: true,
+							ForceNew: true,
+						},
+						"password": &schema.Schema{
+							Description: `Protects the private key. Must have more than 8 characters, less than 15 characters,
+			at least one upper case letter, at least one lower case letter, at least one digit,
+			and can only contain [A-Z][a-z][0-9]_#`,
+							Type:      schema.TypeString,
+							Optional:  true,
+							ForceNew:  true,
+							Sensitive: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func resourceEndpointCertificateCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	log.Printf("[DEBUG] Beginning CreateEndpointCertificate create")
+	log.Printf("[DEBUG] Missing CreateEndpointCertificate create on Cisco ISE. It will only be create it on Terraform")
+	client := m.(*isegosdk.Client)
+
+	var diags diag.Diagnostics
+	log.Printf("[DEBUG] Selected method: CreateEndpointCertificate")
+	request1 := expandRequestEndpointCertificateCreateEndpointCertificate(ctx, "parameters", d)
+
+	response1, _, err := client.EndpointCertificate.CreateEndpointCertificate(request1)
+
+	if err != nil {
+		diags = append(diags, diagError(
+			"Failure when executing CreateEndpointCertificate", err))
+		return diags
+	}
+
+	log.Printf("[DEBUG] Retrieved response")
+
+	vvDirpath := d.Get("dirpath").(string)
+	err = response1.SaveDownload(vvDirpath)
+	if err != nil {
+		diags = append(diags, diagError(
+			"Failure when downloading file", err))
+		return diags
+	}
+	log.Printf("[DEBUG] Downloaded file %s", vvDirpath)
+	_ = d.Set("last_updated", getUnixTimeString())
+
+	d.SetId(getUnixTimeString())
+	return resourceEndpointCertificateRead(ctx, d, m)
+}
+
+func resourceEndpointCertificateRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	return diags
+}
+
+func resourceEndpointCertificateDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	log.Printf("[DEBUG] Beginning EndpointCertificate delete for id=[%s]", d.Id())
+	var diags diag.Diagnostics
+	log.Printf("[DEBUG] Missing EndpointCertificate delete on Cisco ISE. It will only be delete it on Terraform id=[%s]", d.Id())
+	return diags
+}
+
+func expandRequestEndpointCertificateCreateEndpointCertificate(ctx context.Context, key string, d *schema.ResourceData) *isegosdk.RequestEndpointCertificateCreateEndpointCertificate {
+	request := isegosdk.RequestEndpointCertificateCreateEndpointCertificate{}
+	request.ERSEndPointCert = expandRequestEndpointCertificateCreateEndpointCertificateERSEndPointCert(ctx, key, d)
+	return &request
+}
+
+func expandRequestEndpointCertificateCreateEndpointCertificateERSEndPointCert(ctx context.Context, key string, d *schema.ResourceData) *isegosdk.RequestEndpointCertificateCreateEndpointCertificateERSEndPointCert {
+	request := isegosdk.RequestEndpointCertificateCreateEndpointCertificateERSEndPointCert{}
+	if v, ok := d.GetOkExists(fixKeyAccess(key + ".cert_template_name")); !isEmptyValue(reflect.ValueOf(d.Get(fixKeyAccess(key+".cert_template_name")))) && (ok || !reflect.DeepEqual(v, d.Get(fixKeyAccess(key+".cert_template_name")))) {
+		request.CertTemplateName = interfaceToString(v)
+	}
+	if v, ok := d.GetOkExists(fixKeyAccess(key + ".format")); !isEmptyValue(reflect.ValueOf(d.Get(fixKeyAccess(key+".format")))) && (ok || !reflect.DeepEqual(v, d.Get(fixKeyAccess(key+".format")))) {
+		request.Format = interfaceToString(v)
+	}
+	if v, ok := d.GetOkExists(fixKeyAccess(key + ".password")); !isEmptyValue(reflect.ValueOf(d.Get(fixKeyAccess(key+".password")))) && (ok || !reflect.DeepEqual(v, d.Get(fixKeyAccess(key+".password")))) {
+		request.Password = interfaceToString(v)
+	}
+	if v, ok := d.GetOkExists(fixKeyAccess(key + ".certificate_request")); !isEmptyValue(reflect.ValueOf(d.Get(fixKeyAccess(key+".certificate_request")))) && (ok || !reflect.DeepEqual(v, d.Get(fixKeyAccess(key+".certificate_request")))) {
+		request.CertificateRequest = expandRequestEndpointCertificateCreateEndpointCertificateERSEndPointCertCertificateRequest(ctx, key+".certificate_request.0", d)
+	}
+	return &request
+}
+
+func expandRequestEndpointCertificateCreateEndpointCertificateERSEndPointCertCertificateRequest(ctx context.Context, key string, d *schema.ResourceData) *isegosdk.RequestEndpointCertificateCreateEndpointCertificateERSEndPointCertCertificateRequest {
+	request := isegosdk.RequestEndpointCertificateCreateEndpointCertificateERSEndPointCertCertificateRequest{}
+	if v, ok := d.GetOkExists(fixKeyAccess(key + ".san")); !isEmptyValue(reflect.ValueOf(d.Get(fixKeyAccess(key+".san")))) && (ok || !reflect.DeepEqual(v, d.Get(fixKeyAccess(key+".san")))) {
+		request.San = interfaceToString(v)
+	}
+	if v, ok := d.GetOkExists(fixKeyAccess(key + ".cn")); !isEmptyValue(reflect.ValueOf(d.Get(fixKeyAccess(key+".cn")))) && (ok || !reflect.DeepEqual(v, d.Get(fixKeyAccess(key+".cn")))) {
+		request.Cn = interfaceToString(v)
+	}
+	return &request
+}
---
+++ ciscoise/resource_guest_user_reinstate.go
@@ -0,0 +1,152 @@
+package ciscoise
+
+import (
+	"context"
+
+	"log"
+
+	isegosdk "github.com/CiscoISE/ciscoise-go-sdk/sdk"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceGuestUserReinstate() *schema.Resource {
+	return &schema.Resource{
+		Description: `It performs update operation on GuestUser.
+- This data source action allows the client to reinstate a guest user by name.
+- This data source action allows the client to reinstate a guest user by ID.
+`,
+
+		CreateContext: resourceGuestUserReinstateCreate,
+		ReadContext:   resourceGuestUserReinstateRead,
+		DeleteContext: resourceGuestUserReinstateDelete,
+
+		Schema: map[string]*schema.Schema{
+			"last_updated": &schema.Schema{
+				Description: `Unix timestamp records the last time that the resource was updated.`,
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"item": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"parameters": &schema.Schema{
+				Type:     schema.TypeList,
+				Required: true,
+				MaxItems: 1,
+				MinItems: 1,
+				ForceNew: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": &schema.Schema{
+							Description: `id path parameter.`,
+							Type:        schema.TypeString,
+							Optional:    true,
+							ForceNew:    true,
+						},
+						"name": &schema.Schema{
+							Description: `name path parameter.`,
+							Type:        schema.TypeString,
+							Optional:    true,
+							ForceNew:    true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func resourceGuestUserReinstateCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	log.Printf("[DEBUG] Beginning ReinstateGuestUserByName create")
+	log.Printf("[DEBUG] Missing ReinstateGuestUserByName create on Cisco ISE. It will only be create it on Terraform")
+	client := m.(*isegosdk.Client)
+
+	var diags diag.Diagnostics
+	vName, okName := d.GetOk("parameters.0.name")
+	vID, okID := d.GetOk("parameters.0.id")
+
+	method1 := []bool{okName}
+	log.Printf("[DEBUG] Selecting method. Method 1 %v", method1)
+	method2 := []bool{okID}
+	log.Printf("[DEBUG] Selecting method. Method 2 %v", method2)
+
+	selectedMethod := pickMethod([][]bool{method1, method2})
+	if selectedMethod == 1 {
+		log.Printf("[DEBUG] Selected method: ReinstateGuestUserByName")
+		vvName := vName.(string)
+
+		response1, err := client.GuestUser.ReinstateGuestUserByName(vvName)
+
+		if err != nil || response1 == nil {
+			if response1 != nil {
+				log.Printf("[DEBUG] Retrieved error response %s", response1.String())
+				diags = append(diags, diagErrorWithAltAndResponse(
+					"Failure when executing ReinstateGuestUserByName", err, response1.String(),
+					"Failure at ReinstateGuestUserByName, unexpected response", ""))
+				return diags
+			}
+			diags = append(diags, diagErrorWithAlt(
+				"Failure when executing ReinstateGuestUserByName", err,
+				"Failure at ReinstateGuestUserByName, unexpected response", ""))
+			return diags
+		}
+
+		log.Printf("[DEBUG] Retrieved response %s", response1.String())
+
+		if err := d.Set("item", response1.String()); err != nil {
+			diags = append(diags, diagError(
+				"Failure when setting ReinstateGuestUserByName response",
+				err))
+			return diags
+		}
+		d.SetId(getUnixTimeString())
+		_ = d.Set("last_updated", getUnixTimeString())
+		return diags
+
+	}
+	if selectedMethod == 2 {
+		log.Printf("[DEBUG] Selected method: ReinstateGuestUserByID")
+		vvID := vID.(string)
+
+		response2, err := client.GuestUser.ReinstateGuestUserByID(vvID)
+
+		if err != nil || response2 == nil {
+			diags = append(diags, diagErrorWithAlt(
+				"Failure when executing ReinstateGuestUserByID", err,
+				"Failure at ReinstateGuestUserByID, unexpected response", ""))
+			return diags
+		}
+
+		log.Printf("[DEBUG] Retrieved response %s", response2.String())
+
+		if err := d.Set("item", response2.String()); err != nil {
+			diags = append(diags, diagError(
+				"Failure when setting ReinstateGuestUserByID response",
+				err))
+			return diags
+		}
+		d.SetId(getUnixTimeString())
+		_ = d.Set("last_updated", getUnixTimeString())
+		return diags
+
+	}
+	_ = d.Set("last_updated", getUnixTimeString())
+
+	d.SetId(getUnixTimeString())
+	return resourceGuestUserReinstateRead(ctx, d, m)
+}
+
+func resourceGuestUserReinstateRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	return diags
+}
+
+func resourceGuestUserReinstateDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	log.Printf("[DEBUG] Beginning GuestUserReinstate delete for id=[%s]", d.Id())
+	var diags diag.Diagnostics
+	log.Printf("[DEBUG] Missing GuestUserReinstate delete on Cisco ISE. It will only be delete it on Terraform id=[%s]", d.Id())
+	return diags
+}
