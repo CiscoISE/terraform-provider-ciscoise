2d06ceb7c4450d606af97d16f547c3b80f43100f
fmunoz <fmunoz@altus.cr>
2022-04-26 09:28:48 -0600

    resource tested: trustsec_vn_bulk_create trustsec_vn_bulk_delete trustsec_vn_bulk_update

--- newResources/ciscoise_trustsec_vn_bulk_update/resource.tf
+++ newResources/ciscoise_trustsec_vn_bulk_update/resource.tf
@@ -1,12 +1,16 @@
 
 resource "ciscoise_trustsec_vn_bulk_update" "example" {
   provider = ciscoise
+  lifecycle {
+    create_before_destroy = true
+  }
+  parameters{
+    payload {
 
-  payload {
-
-    additional_attributes = "string"
-    id                    = "string"
-    last_update           = "string"
-    name                  = "string"
+      additional_attributes = "string"
+      id                    = "string"
+      last_update           = "string"
+      name                  = "string"
+    }
   }
 }
