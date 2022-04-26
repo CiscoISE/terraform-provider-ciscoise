1be9979ac6ce471240567d9a929cad0652c25a3c
fmunoz <fmunoz@altus.cr>
2022-04-26 09:02:06 -0600

    resources tested: trustsec_sg_vn_mapping_bulk_create trustsec_sg_vn_mapping_bulk_delete

--- examples/samples/resources/ciscoise_trustsec_sg_vn_mapping_bulk_delete/resource.tf
+++ examples/samples/resources/ciscoise_trustsec_sg_vn_mapping_bulk_delete/resource.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    ciscoise = {
+      version = "0.3.0-beta"
+      source  = "hashicorp.com/edu/ciscoise"
+    }
+  }
+}
 
 resource "ciscoise_trustsec_sg_vn_mapping_bulk_delete" "example" {
   provider = ciscoise
