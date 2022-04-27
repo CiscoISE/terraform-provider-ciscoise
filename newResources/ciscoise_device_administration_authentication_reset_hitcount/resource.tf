236cae59e09d501eb012e5b54a770238d1a0bd87
fmunoz <fmunoz@altus.cr>
2022-04-27 13:35:44 -0600

    resources tested: ciscoise_device_administration_authorization_reset_hitcount ciscoise_device_administration_authentication_reset_hitcount with 404 policyID not found

--- newResources/ciscoise_device_administration_authentication_reset_hitcount/resource.tf
+++ newResources/ciscoise_device_administration_authentication_reset_hitcount/resource.tf
@@ -1,6 +1,11 @@
 
 resource "ciscoise_device_administration_authentication_reset_hitcount" "example" {
   provider  = ciscoise
-  policy_id = "string"
+  lifecycle {
+    create_before_destroy = true
+  }
+  parameters{
+    policy_id = "string"
+  }
 
 }
