09a762badb79da5744b4e1f9dfe78d3da2df3b93
fmunoz <fmunoz@altus.cr>
2022-04-27 13:35:44 -0600

    resources tested: ciscoise_device_administration_authorization_reset_hitcount ciscoise_device_administration_authentication_reset_hitcount with 404 policyID not found

---
+++ examples/samples/resources/ciscoise_device_administration_authorization_reset_hitcount/resource.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    ciscoise = {
+      version = "0.3.0-beta"
+      source  = "hashicorp.com/edu/ciscoise"
+    }
+  }
+}
+
+
+resource "ciscoise_device_administration_authorization_reset_hitcount" "example" {
+  provider  = ciscoise
+  lifecycle {
+    create_before_destroy = true
+  }
+  parameters{
+    policy_id = "1a0e4fb0-c56f-11ec-ba31-463ee0624939"
+  }
+
+}
--- newResources/ciscoise_device_administration_authorization_reset_hitcount/resource.tf
+++ newResources/ciscoise_device_administration_authorization_reset_hitcount/resource.tf
@@ -1,6 +1,11 @@
 
 resource "ciscoise_device_administration_authorization_reset_hitcount" "example" {
   provider  = ciscoise
-  policy_id = "string"
+  lifecycle {
+    create_before_destroy = true
+  }
+  parameters{
+    policy_id = "string"
+  }
 
 }
