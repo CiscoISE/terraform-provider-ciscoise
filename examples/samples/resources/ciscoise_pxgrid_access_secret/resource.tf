94e3860a77d18a2905914fe7c7f003c964c622f9
fmunoz <fmunoz@altus.cr>
2022-04-27 12:26:58 -0600

    resource tested: ciscoise_pxgrid_access_secret with timeout

--- examples/samples/resources/ciscoise_pxgrid_access_secret/resource.tf
+++ examples/samples/resources/ciscoise_pxgrid_access_secret/resource.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    ciscoise = {
+      version = "0.3.0-beta"
+      source  = "hashicorp.com/edu/ciscoise"
+    }
+  }
+}
 
 resource "ciscoise_pxgrid_access_secret" "example" {
   provider = ciscoise
