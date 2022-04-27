e8efde9f5b3fd96e41f1960c4cb3b0f18c2d0a9b
fmunoz <fmunoz@altus.cr>
2022-04-27 14:25:02 -0600

    resource tested: active_directory_leave_domain active_directory_leave_domain_with_all_nodes

--- examples/samples/resources/ciscoise_active_directory_join_domain_with_all_nodes/resource.tf
+++ examples/samples/resources/ciscoise_active_directory_join_domain_with_all_nodes/resource.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    ciscoise = {
+      version = "0.3.0-beta"
+      source  = "hashicorp.com/edu/ciscoise"
+    }
+  }
+}
 
 resource "ciscoise_active_directory_join_domain_with_all_nodes" "example" {
   provider = ciscoise
