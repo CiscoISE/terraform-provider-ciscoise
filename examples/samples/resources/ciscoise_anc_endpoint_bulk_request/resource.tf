fe2c47710f5e165002ec16ccf494347b754f0708
fmunoz <fmunoz@altus.cr>
2022-04-27 14:23:52 -0600

    resource tested with schema validation error ciscoise_anc_endpoint_bulk_request

--- examples/samples/resources/ciscoise_anc_endpoint_bulk_request/resource.tf
+++ examples/samples/resources/ciscoise_anc_endpoint_bulk_request/resource.tf
@@ -1,4 +1,11 @@
-
+terraform {
+  required_providers {
+    ciscoise = {
+      version = "0.3.0-beta"
+      source  = "hashicorp.com/edu/ciscoise"
+    }
+  }
+}
 resource "ciscoise_anc_endpoint_bulk_request" "example" {
   provider = ciscoise
   lifecycle {
