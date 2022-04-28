b72065731aacaeff996b7cebd0162d1137733d91
Bryan Vargas <bvargas@altus.cr>
2022-04-28 14:14:00 -0600

    Change value params to string

--- newResources/ciscoise_guest_user_deny/resource.tf
+++ newResources/ciscoise_guest_user_deny/resource.tf
@@ -4,6 +4,6 @@ resource "ciscoise_guest_user_deny" "example" {
     create_before_destroy = true
   }
   parameters {
-    id       = "872c4e3f-8b3f-4462-98ba-00519a033cce"
+    id       = "string"
   }
 }
--- newResources/ciscoise_network_access_authentication_rules_reset_hitcount/resource.tf
+++ newResources/ciscoise_network_access_authentication_rules_reset_hitcount/resource.tf
@@ -4,6 +4,6 @@ resource "ciscoise_network_access_authentication_rules_reset_hitcount" "example"
     create_before_destroy = true
   }
   parameters {
-    policy_id = "70836740-8bff-11e6-996c-525400b48521"
+    policy_id = "string"
   } 
 }
