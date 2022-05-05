93afa532a957c8fda4ffaa3974baadb6e235c4f5
fmunoz <fmunoz@altus.cr>
2022-05-05 12:13:12 -0600

    adding ciscoise_system_certificate_import resource

---
+++ examples/samples/resources/ciscoise_system_certificate_import/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    ciscoise = {
+      version = "0.4.0-beta"
+      source  = "hashicorp.com/edu/ciscoise"
+    }
+  }
+}
+
+provider "ciscoise" {
+}
+
+
+resource "ciscoise_system_certificate_import" "response" {
+  provider = ciscoise
+  parameters {
+    admin                                      = "false"
+    allow_extended_validity                    = "false"
+    allow_out_of_date_cert                     = "false"
+    allow_portal_tag_transfer_for_same_subject = "false"
+    allow_replacement_of_certificates          = "false"
+    allow_replacement_of_portal_group_tag      = "false"
+    allow_role_transfer_for_same_subject       = "false"
+    allow_sha1_certificates                    = "false"
+    allow_wild_card_certificates               = "false"
+    data                                       = "string"
+    eap                                        = "false"
+    ims                                        = "false"
+
+    name                            = "string"
+    password                        = "******"
+    portal                          = "false"
+    portal_group_tag                = "string"
+    private_key_data                = "string"
+    pxgrid                          = "false"
+    radius                          = "false"
+    saml                            = "false"
+    validate_certificate_extensions = "false"
+  }
+}
+
