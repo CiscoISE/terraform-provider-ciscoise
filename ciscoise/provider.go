1ae2fe813c120ba5338300aa3edeaed93367633b
fmunoz <fmunoz@altus.cr>
2022-05-05 15:58:24 -0600

    adding ciscoise_system_certificate_import resource

--- ciscoise/provider.go
+++ ciscoise/provider.go
@@ -72,6 +72,7 @@ func Provider() *schema.Provider {
 			},
 		},
 		ResourcesMap: map[string]*schema.Resource{
+			"ciscoise_system_certificate_import":                                   resourceSystemCertificateImport(),
 			"ciscoise_trusted_certificate_import":                                  resourceTrustedCertificateImport(),
 			"ciscoise_node_standalone_to_primary":                                  resourceNodeStandaloneToPrimary(),
 			"ciscoise_node_secondary_to_primary":                                   resourceNodeSecondaryToPrimary(),
