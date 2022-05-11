3cde5972a0c11ddfe3962f73d2cf573e8fe4e13b
fmunoz <fmunoz@altus.cr>
2022-05-11 11:55:56 -0600

    Including an example sample of ciscoise_tacacs_command_sets

---
+++ examples/samples/resources/ciscoise_tacacs_command_sets/resource.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    ciscoise = {
+      version = "0.5.0-beta"
+      source  = "hashicorp.com/edu/ciscoise"
+    }
+  }
+}
+resource "ciscoise_tacacs_command_sets" "asa_read_only" {
+  parameters {
+    description      = "ASA Read Only"
+    name             = "ASA Operator"
+    permit_unmatched = "false"
+    commands {
+      command_list {
+        grant     = "PERMIT"
+        command   = "show"
+        arguments = ""
+      }
+    }
+  }
+}
