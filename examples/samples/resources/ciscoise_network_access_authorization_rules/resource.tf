0b7b7732077f3c2b26d241b43e47fdb3380e7bbe
fmunoz <fmunoz@altus.cr>
2022-10-03 13:39:46 -0600

    Resource ciscoise_network_access_authorization_rules fails #55, (Can't test it)

---
+++ examples/samples/resources/ciscoise_network_access_authorization_rules/resource.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    ciscoise = {
+      version = "0.6.7-beta"
+      source  = "hashicorp.com/edu/ciscoise"
+    }
+  }
+}
+
+provider "ciscoise" {
+}
+
+resource "ciscoise_network_access_authorization_rules" "example" {
+  provider = ciscoise
+  parameters {
+    profile   = ["authp_uc6"]
+    rule {
+      condition {
+        condition_type = "ConditionReference"
+        name           = "lc_uc6"
+        id             = "716b2dfd-a1e0-4d94-9802-eb4d3bc614d3"
+        is_negate      = "true"
+      }
+      default    = "true"
+      # hit_counts = 1
+      # id         = "716b2dfd-a1e0-4d94-9802-eb4d3bc614d3"
+      name       = "MyRuleName_1"
+      rank       = 1
+      state      = "enabled"
+    }
+    security_group = "BYOD"
+  }
+}
