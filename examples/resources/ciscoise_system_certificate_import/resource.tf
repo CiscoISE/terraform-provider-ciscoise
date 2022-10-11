822b3e5483c1c8a4f74ac1946e48941cd38220ef
fmunoz <fmunoz@altus.cr>
2022-10-11 12:26:39 -0600

    Updating system_certificate_import resource documentation

--- examples/resources/ciscoise_system_certificate_import/resource.tf
+++ examples/resources/ciscoise_system_certificate_import/resource.tf
@@ -1,14 +1,28 @@
 
-resource "ciscoise_backup_restore" "example" {
+resource "ciscoise_system_certificate" "example" {
   provider = ciscoise
-  lifecycle {
-    create_before_destroy = true
-  }
   parameters {
-    backup_encryption_key = "string"
-    repository_name       = "string"
-    restore_file          = "string"
-    restore_include_adeos = "string"
+    admin                                      = "false"
+    allow_portal_tag_transfer_for_same_subject = "false"
+    allow_replacement_of_portal_group_tag      = "false"
+    allow_role_transfer_for_same_subject       = "false"
+    description                                = "string"
+    eap                                        = "false"
+    expiration_ttl_period                      = 1
+    expiration_ttl_units                       = "string"
+    host_name                                  = "string"
+    id                                         = "string"
+    ims                                        = "false"
+    name                                       = "string"
+    portal                                     = "false"
+    portal_group_tag                           = "string"
+    pxgrid                                     = "false"
+    radius                                     = "false"
+    renew_self_signed_certificate              = "false"
+    saml                                       = "false"
   }
+}
 
+output "ciscoise_system_certificate_example" {
+  value = ciscoise_system_certificate.example
 }
